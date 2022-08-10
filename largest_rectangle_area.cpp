bd8b06e7edde24000dad6b01f2e7d5c0f6dcf409
Fabian Hahn <fabian@hahn.graphics>
2022-08-10 12:51:19 +0100

    fast solution

--- largest_rectangle_area.cpp
+++ largest_rectangle_area.cpp
@@ -10,43 +10,66 @@ using ::testing::IsEmpty;
 using ::testing::UnorderedElementsAre;
 
 int largestRectangleArea(const std::vector<int>& heights) {
-  constexpr auto kMaxHeight = 10000;
+  if (heights.empty()) {
+    return 0;
+  }
+  if (heights.size() == 1) {
+    return heights[0];
+  }
 
-  std::vector<int> sizes(kMaxHeight + 1, 0);
+  std::vector<std::pair<int, int>> stack;
   int largest = 0;
-  int previous = 0;
   for (int i = 0; i < heights.size(); i++) {
     int current = heights[i];
 
-    for (int j = 1; j <= current; j++) {
-      sizes[j]++;
-    }
-
-    if (current < previous) {
-      for (int j = current + 1; j <= previous; j++) {
-        if (sizes[j] * j > largest) {
-          largest = sizes[j] * j;
-        }
-
-        sizes[j] = 0;
+    int width = 0;
+    while (!stack.empty()) {
+      int previous = stack.back().first;
+      int previousWidth = stack.back().second;
+      if (previous <= current) {
+        break;
       }
+
+      width += previousWidth;
+      if (width * previous > largest) {
+        largest = width * previous;
+      }
+
+      stack.pop_back();
     }
 
-    previous = current;
+    int previous = 0;
+    if (!stack.empty()) {
+      previous = stack.back().first;
+    }
+
+    if (!stack.empty() && current == previous) {
+      stack.back().second += width + 1;
+    } else {
+      assert(stack.empty() || current > previous);
+      stack.emplace_back(std::make_pair(current, width + 1));
+    }
   }
 
-  for (int j = 0; j <= kMaxHeight; j++) {
-    if (sizes[j] * j > largest) {
-      largest = sizes[j] * j;
+  int width = 0;
+  while (!stack.empty()) {
+    int previous = stack.back().first;
+    int previousWidth = stack.back().second;
+
+    width += previousWidth;
+
+    if (width * previous > largest) {
+      largest = width * previous;
     }
 
-    sizes[j] = 0;
+    stack.pop_back();
   }
 
   return largest;
 }
 
 TEST(LargestRectangleArea, test) {
+  EXPECT_EQ(largestRectangleArea(std::vector<int>{2, 1, 2}), 3);
   EXPECT_EQ(largestRectangleArea(std::vector<int>{2, 1, 5, 6, 2, 3}), 10);
   EXPECT_EQ(largestRectangleArea(std::vector<int>{2, 4}), 4);
   EXPECT_EQ(largestRectangleArea(std::vector<int>(42858, 8180)), 42858 * 8180);
